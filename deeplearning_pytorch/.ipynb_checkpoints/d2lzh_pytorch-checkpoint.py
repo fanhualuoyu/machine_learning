99f8a98e7b3c5df27074e723dcb7f66891fa19ff
commit01 <1260626100@qq.com>
2020-12-23 21:52:34 +0800

    2020.12.23

--- deeplearning_pytorch/.ipynb_checkpoints/d2lzh_pytorch-checkpoint.py
+++ deeplearning_pytorch/.ipynb_checkpoints/d2lzh_pytorch-checkpoint.py
@@ -5,6 +5,7 @@ from matplotlib import pyplot as plt
 import numpy as np
 import random
 import sys
+import time
 import torchvision
 import torchvision.transforms as transforms
 def use_svg_display():
@@ -60,7 +61,10 @@ def load_data_fashion_mnist(batch_size): # 加载数据集
     test_iter = torch.utils.data.DataLoader(mnist_test, batch_size=batch_size, shuffle=False, num_workers=num_workers)
     return train_iter, test_iter
 
-def evalute_accuracy(data_iter, net): # 计算准确率
+def evalute_accuracy(data_iter, net, device=None): # 计算准确率
+    if device is None and isinstance(net, torch.nn.Module):
+        # 如果没指定device就使用net的device
+        device = list(net.parameters())[0].device
     acc_sum, n = 0.0, 0
     for X, y in data_iter:
         if isinstance(net, torch.nn.Module):
@@ -113,3 +117,56 @@ def semilogy(x_vals, y_vals, x_label, y_label, x2_vals=None, y2_vals=None, legen
     if x2_vals and y2_vals:
         plt.semilogy(x2_vals, y2_vals)
         plt.legend(legend)
+
+def corr2d(X, K): # 二维相关运算
+    h, w = K.shape
+    Y = torch.zeros((X.shape[0] - h + 1, X.shape[1] - w + 1))
+    for i in range(Y.shape[0]):
+        for j in range(Y.shape[1]):
+            Y[i, j] = (X[i: i+h, j: j+w] * K).sum()
+    return Y
+
+def train_ch5(net, train_iter, test_iter, batch_size, optimizer, device, num_epochs):
+    net = net.to(device)
+    print("training to ", device)
+    loss = torch.nn.CrossEntropyLoss()
+    for epoch in range(num_epochs):
+        train_l_sum, train_acc_sum, n, batch_count, start = 0.0, 0.0, 0, 0, time.time()
+        for X, y in train_iter:
+            X = X.to(device)
+            y = y.to(device)
+            y_hat = net(X)
+            l = loss(y_hat, y)
+            optimizer.zero_grad()
+            l.backward()
+            optimizer.step()
+            train_l_sum += l.cpu().item()
+            train_acc_sum += (y_hat.argmax(dim=1) == y).sum().cpu().item()
+            n += y.shape[0]
+            batch_count += 1
+        test_acc = evalute_accuracy(test_iter, net)
+        print('epoch %d, loss %.4f, train acc %.3f, test acc %.3f, time %.1f sec' 
+               % (epoch + 1, train_l_sum / batch_count, train_acc_sum / n, test_acc, time.time() - start))
+        
+def load_data_fashion_mnist(batch_size, resize=None, root=r'D:\web\python\deeplearning_pytorch\Datasets/FashionMNIST'):
+    trans = []
+    if resize:
+        trans.append(torchvision.transforms.Resize(size=resize))
+    trans.append(torchvision.transforms.ToTensor())
+
+    transform = torchvision.transforms.Compose(trans)
+    mnist_train = torchvision.datasets.FashionMNIST(root=root, train=True, download=True, transform=transform)
+    mnist_test = torchvision.datasets.FashionMNIST(root=root, train=False, download=True, transform=transform)
+
+    train_iter = torch.utils.data.DataLoader(mnist_train, batch_size=batch_size, shuffle=True, num_workers=4)
+    test_iter = torch.utils.data.DataLoader(mnist_test, batch_size=batch_size, shuffle=False, num_workers=4)
+
+    return train_iter, test_iter
+
+import torch.nn.functional as F
+class GlobalAvgPool2d(nn.Module):
+    # 全局平均池化层可通过将池化窗口形状设置成输入的高和宽实现
+    def __init__(self):
+        super(GlobalAvgPool2d, self).__init__()
+    def forward(self, x):
+        return F.avg_pool2d(x, kernel_size=x.size()[2:])
